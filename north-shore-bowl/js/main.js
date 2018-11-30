9058e711248bb345e4bd5ebd937374dec46db6be
alexbohn85 <alexbohn85@gmail.com>
2018-11-29 20:19:21 -0800

    mobile header finished 6

--- north-shore-bowl/js/main.js
+++ north-shore-bowl/js/main.js
@@ -1,9 +1,17 @@
 function toggleMenu(event){
+	
 	let element = document.getElementById("menu_mobile_id");
+	let btn = document.getElementById("header_icon");
+
 	if(element.style.display === 'none'){
 		element.style.display = 'block';
+		btn.style.textShadow = '0px 0px 1px black';
+		btn.style.color = '#458';
 		element.classList.add("menu_mobile");
 	} else{
 		element.style.display = 'none';
+		btn.style.color = '#87c';
+		btn.style.textShadow = '';
+
 	}
 }
