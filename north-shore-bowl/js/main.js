2e8e302d98392eb2ef48c5ad957dc01d75283866
alexbohn85 <alexbohn85@gmail.com>
2018-11-30 00:21:23 -0800

    mobile header finished 11

--- north-shore-bowl/js/main.js
+++ north-shore-bowl/js/main.js
@@ -4,21 +4,6 @@ function toggleMenu(event){
 	let element = document.getElementById("menu_mobile_id");
 	let btn = document.getElementById("header_icon");
 
-	// if(!element.classList.contains("menu_mobile")){
-	// 	element.style.display = 'block';
-	// 	element.classList.add("menu_mobile");
-	// 	btn.style.textShadow = '0px 0px 1px black';
-	// 	btn.style.color = '#458';	
-
-	// } else{
-	// 	element.style.display = 'none';
-	// 	btn.style.color = '#87c';
-	// 	btn.style.textShadow = '';
-	// 	element.classList.remove("menu_mobile");
-	// 	element.style.animationDirection = 'reverse';
-	// 	element.classList.add("menu_mobile");
-	// }
-
 	if(toggle){
 		element.style.animationDirection = 'normal';
 		element.classList.add("menu_mobile");
@@ -28,12 +13,8 @@ function toggleMenu(event){
 		btn.style.transform = 'rotateZ(90deg)';
 
 	} else{
-		btn.style.transform = 'rotateZ(0deg)';
 		element.classList.remove("menu_mobile");
-		element.style.animationDirection = 'reverse';
-		element.classList.add("menu_mobile");
-		
-		// btn.style.color = '#000';
+		btn.style.transform = 'rotateZ(0deg)';
 		btn.style.textShadow = '';
 	}
 
