e999d7b154d438140285c34ac05ed53960737ed5
alexbohn85 <alexbohn85@gmail.com>
2018-11-29 18:33:41 -0800

    mobile first menu design

--- north-shore-bowl/js/main.js
+++ north-shore-bowl/js/main.js
@@ -1,3 +1,9 @@
-function start(){
-	
+function toggleMenu(event){
+	let element = document.getElementById("menu_mobile_id");
+	if(element.style.display === 'none'){
+		element.style.display = 'block';
+		element.classList.add("menu_mobile");
+	} else{
+		element.style.display = 'none';
+	}
 }
