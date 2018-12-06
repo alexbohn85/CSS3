f33b845600fe30d8e20209e490d1f81ae85a6969
alexbohn85 <alexbohn85@gmail.com>
2018-12-06 02:25:05 -0800

    add onscroll for wrapper

--- pizzeria/js/main.js
+++ pizzeria/js/main.js
@@ -5,24 +5,12 @@ window.onscroll = function(){
 
 	let element_topPos = document.getElementById("aboutus").getBoundingClientRect().top;
 	console.log(element_topPos);
+};
 
-	// let products = document.getElementsByClassName('products')[0].getBoundingClientRect().top;
-	// console.log(products);
+document.getElementsByClassName("wrapper")[0].onscroll = function(){
+	var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+	// console.log(scrollTop);
 
-	// if(products < 600){
-	// 	document.getElementsByTagName('article')[0].classList.add('showPictures');
-	// }else{
-	// 	document.getElementsByTagName('article')[0].classList.remove('showPictures');
-	// }
-	// if(products < 400){		
-	// 	document.getElementsByTagName('article')[1].classList.add('showPictures');
-	// }else{
-	// 	document.getElementsByTagName('article')[1].classList.remove('showPictures');
-	// }
-	// if(products < 200){
-	// 	document.getElementsByTagName('article')[2].classList.add('showPictures');
-	// }else{
-	// 	document.getElementsByTagName('article')[2].classList.remove('showPictures');
-	// }
-
-}; 
+	let element_topPos = document.getElementById("aboutus").getBoundingClientRect().top;
+	console.log(element_topPos);
+}
